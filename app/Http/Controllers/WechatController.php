5ac8facd4c55bc8b6980a9d0b311747f8842da5e
maple <copyrenzhe@gmail.com>
2016-07-15 11:20:53 +0800

    init wechat

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -14,14 +14,10 @@ class WechatController extends Controller
      */
     public function serve()
     {
-        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
-
         $wechat = app('wechat');
         $wechat->server->setMessageHandler(function($message){
             return "欢迎关注 overtrue！";
         });
-
-        Log::info('return response.');
 
         return $wechat->server->serve();
     }
