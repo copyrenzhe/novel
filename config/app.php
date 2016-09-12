272d78bf19da357d79fa0dc4c67cd2a7566e75e5
maple <copyrenzhe@gmail.com>
2016-09-12 14:43:30 +0800

    add laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -163,6 +163,7 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Acacha\AdminLTETemplateLaravel\Providers\AdminLTETemplateServiceProvider::class,
         Yajra\Datatables\DatatablesServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -216,6 +217,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'AdminLTE' => Acacha\AdminLTETemplateLaravel\Facades\AdminLTE::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
