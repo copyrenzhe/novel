9c53c2ab0e94a759eaec1f228f7810112d9c38b0
maple <copyrenzhe@gmail.com>
2016-09-16 02:15:24 +0800

    read, write

--- config/database.php
+++ config/database.php
@@ -54,7 +54,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'read' => [
+                'host' => env('DB_HOST_READ', env('DB_HOST', 'localhost')),
+            ],
+            'write' => [
+                'host' => env('DB_HOST_WRITE', env('DB_HOST', 'localhost')),
+            ],
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
