ab4b666e014d7937967640e07d9a3b722fa3ea7f
maple <copyrenzhe@gmail.com>
2016-10-10 00:07:05 +0800

    update snatch

--- database/seeds/RepairNovelSeeder.php
+++ database/seeds/RepairNovelSeeder.php
@@ -14,7 +14,7 @@ class RepairNovelSeeder extends Seeder
     {
         $dtStart = microtime_float();
         Log::useDailyFiles(storage_path().'/logs/repair', 5);
-        for ($id =6000; $id<8000; $id++){
+        for ($id =8000; $id<10000; $id++){
             dispatch(new SnatchRepair($id, true));
         }
         $continueEnd = microtime_float();
