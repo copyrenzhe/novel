dbe7cc6cd6d380e7b62b3a59391c9da795182d66
maple <copyrenzhe@gmail.com>
2016-10-12 21:05:26 +0800

    update repair data

--- database/seeds/RepairNovelSeeder.php
+++ database/seeds/RepairNovelSeeder.php
@@ -14,7 +14,7 @@ class RepairNovelSeeder extends Seeder
     {
         $dtStart = microtime_float();
         Log::useDailyFiles(storage_path().'/logs/repair', 5);
-        for ($id =8000; $id<10000; $id++){
+        for ($id =10000; $id<12000; $id++){
             dispatch(new SnatchRepair($id, true));
         }
         $continueEnd = microtime_float();
