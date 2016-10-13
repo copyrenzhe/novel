3a747b161540a81f2ec5b51f0014e775e959fd16
maple <copyrenzhe@gmail.com>
2016-10-13 19:38:56 +0800

    update snatch update

--- app/functions.php
+++ app/functions.php
@@ -151,3 +151,42 @@ if(!function_exists('is_weixin')) {
         return false;
     }
 }
+
+    /*
+     * ----------------------------------
+     * update batch
+     * ----------------------------------
+     *
+     * multiple update in one query
+     *
+     * tablename( required | string )
+     * multipleData ( required | array of array )
+     */
+if(!function_exists('updateBatch')) {
+    function updateBatch($tableName = "", $multipleData = array()){
+        if( $tableName && !empty($multipleData) ) {
+            // column or fields to update
+            $updateColumn = array_keys($multipleData[0]);
+            $referenceColumn = $updateColumn[0]; //e.g id
+            unset($updateColumn[0]);
+            $whereIn = "";
+            $q = "UPDATE ".$tableName." SET ";
+            foreach ( $updateColumn as $uColumn ) {
+                $q .=  $uColumn." = CASE ";
+
+                foreach( $multipleData as $data ) {
+                    $q .= "WHEN ".$referenceColumn." = '".$data[$referenceColumn]."' THEN '".$data[$uColumn]."' ";
+                }
+                $q .= "ELSE ".$uColumn." END, ";
+            }
+            foreach( $multipleData as $data ) {
+                $whereIn .= "'".$data[$referenceColumn]."', ";
+            }
+            $q = rtrim($q, ", ")." WHERE ".$referenceColumn." IN (".  rtrim($whereIn, ', ').")";
+            // Update
+            return DB::update(DB::raw($q));
+        } else {
+            return false;
+        }
+    }
+}
