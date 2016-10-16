4d7324c5e6dbe387de44647e8dbc50080acf2931
maple <copyrenzhe@gmail.com>
2016-10-17 00:06:27 +0800

    update snatch

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,18 +33,18 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         //每个小时更新热门小说
-        $schedule->command('snatch:updateHot --queue')
-                ->dailyAt('06:00')
-                ->withoutOverlapping();
+//        $schedule->command('snatch:updateHot --queue')
+//                ->dailyAt('06:00')
+//                ->withoutOverlapping();
 //
         //每天更新所有小说章节
-        $schedule->command('snatch:update --queue')
-                ->dailyAt('03:00')
-                ->withoutOverlapping();
+//        $schedule->command('snatch:update --queue')
+//                ->dailyAt('03:00')
+//                ->withoutOverlapping();
 //
         //每天更新所有小说章节数
-        $schedule->command('sum:chapter --queue')
-                ->dailyAt('12:00');
+//        $schedule->command('sum:chapter --queue')
+//                ->dailyAt('12:00');
 //
 //        //每天发送邮件
 //        $schedule->command('mail:daily')
