ed2c166a9f1afcb66bd86f57d3629422945f61ef
maple <copyrenzhe@gmail.com>
2016-10-23 23:07:56 +0800

    update snatch

--- database/seeds/AlertChapterSourceSeeder.php
+++ database/seeds/AlertChapterSourceSeeder.php
@@ -14,7 +14,7 @@ class AlertChapterSourceSeeder extends Seeder
     public function run()
     {
         $dtStart = microtime_float();
-        $novels = Novel::where('id', '<', 717)->get();
+        $novels = Novel::where('id', '<', 643)->get();
         foreach ($novels as $novel) {
             dispatch(new AlertSource($novel));
         }
