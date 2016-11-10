346df4ec2b9c996f8e96e163089dd65f437008bb
maple <copyrenzhe@gmail.com>
2016-11-10 12:00:05 +0800

    update mzhu

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,8 +17,7 @@ Route::get('test', function(){
 });
 
 Route::get('test2', function(){
-    $job = new \App\Jobs\SnatchInit('http://www.mzhu8.com/book/4/index.html', 'mzhu');
-    $job->handle();
+    @fopen('http://www.mzhu8.com/files/article/image/0/900/900s.jpg', 'r') ? dd ('success') : dd('false');
 });
 
 Route::get('test3', function(){
