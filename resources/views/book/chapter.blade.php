206966d02ebf55f9ac0f0c494e799342e4920a68
zhangpeng <skxtay@gmail.com>
2016-12-15 16:40:51 +0800

    添加详情页预加载

--- resources/views/book/chapter.blade.php
+++ resources/views/book/chapter.blade.php
@@ -4,6 +4,24 @@
 @section('keywords'){{ $chapter->name }},@if($chapter->novel->is_over){{ $chapter->novel->name }}全文完整版,@else{{ $chapter->novel->name }}最新章节,@endif{{ $chapter->novel->name }}无弹窗,书虫网@stop
 @section('description')小说{{ $chapter->novel->name }}正文 {{ $chapter->name }}在线阅读。作者{{ $chapter->novel->author->name }}的小说,{{ $chapter->novel->name }}全文免费阅读到书虫网(www.shu000.com)@stop
 @section('link')<link rel="canonical" href="{{ route('chapter', ['bookId'=> $chapter->novel->id, 'chapterId' => $chapter->id]) }}" />@stop
+@section('js')
+<script>
+    var CHAPTER_INFO = {
+        book_id: "{{ $chapter->novel_id }}",
+        chapter_num: "{{ $chapter->novel->chapter_num }}",
+        chapter_id: "{{ $chapter->id }}",
+        chapter_name: "{{ $chapter->name }}"
+    }
+    $(function () {
+        var chapterHistory = {
+            'id' : CHAPTER_INFO.chapter_id,
+            'href' : window.location.href,
+            'title' : CHAPTER_INFO.chapter_name
+        };
+        $.jStorage.set(CHAPTER_INFO.book_id, chapterHistory);
+    })
+</script>
+@stop
 @section('content')
     @if(isset($js))
     <script>
@@ -20,15 +38,17 @@
         wx.onMenuShareQZone(shareData);
     </script>
     @endif
-<div id="view-page">
+<div class="view-page">
     <!-- Thong tin truyen -->
-    <h1 class="title"><a href="{{ route('book', ['bookId' => $chapter->novel_id]) }}" title="{{ $chapter->novel->name }}">{{ $chapter->novel->name }}</a> / {{ $chapter->name }}</h1>
-    <input type="hidden" name="urlchange" value="{{ route('book', ['bookId' => $chapter->novel_id]) }}">
-    <div class="tool-right">
-        <a href="javascript:setbookmark();" title="Bookmark" class="btn">保存书签</a>
+    <div class="detail-title clearfix">
+        <h1 class="title"><a href="{{ route('book', ['bookId' => $chapter->novel_id]) }}" title="{{ $chapter->novel->name }}">{{ $chapter->novel->name }}</a> / {{ $chapter->name }}</h1>
+        <input type="hidden" name="urlchange" value="{{ route('book', ['bookId' => $chapter->novel_id]) }}">
+        <div class="tool-right">
+            <a href="javascript:setbookmark();" title="Bookmark" class="btn">保存书签</a>
+        </div>
     </div>
-    <div class="box">
-        <div class="content">
+    <div class="detail-box">
+        <div class="content" data-page="{{ $chapter->id }}">
             <h2 class="chapter-number">{{ $chapter->name }}</h2>
             <br class="clr">
             <div class="contents-comic">
@@ -38,32 +58,16 @@
     </div>
 
     <!--/ thong tin truyen -->
-    <br class="clr">
-    <div class="chap-select">
+    <div class="clearfix">
         <div class="flr chap-select-dropdown">
             @if(isset($prev))
-            <a href="{{ route('chapter', ['bookId' => $chapter->novel_id, 'chapterId' => $prev->id]) }}" class="btn-blue">上一章</a>
+            <a href="{{ route('chapter', ['bookId' => $chapter->novel_id, 'chapterId' => $prev->id]) }}" class="btn-blue prev-page">上一章</a>
             @endif
             <a href="{{ route('book', ['bookId' => $chapter->novel_id]) }}" class="btn-blue">目录</a>
             @if(isset($next))
-            <a href="{{ route('chapter', ['bookId' => $chapter->novel_id, 'chapterId' => $next->id]) }}" class="btn-blue">下一章</a>
+            <a href="{{ route('chapter', ['bookId' => $chapter->novel_id, 'chapterId' => $next->id]) }}" class="btn-blue next-page">下一章</a>
             @endif
         </div>
     </div>
 </div>
-@stop
-@section('js')
-<script>
-    var book_id = "{{ $chapter->novel_id }}";
-    var chapter_id = "{{ $chapter->id }}";
-    var chapter_name = '{{ $chapter->name }}';
-    $(function () {
-        var chapterHistory = {
-            'id' : chapter_id,
-            'href' : window.location.href,
-            'title' : chapter_name
-        };
-        $.jStorage.set(book_id, chapterHistory);
-    })
-</script>
 @stop
