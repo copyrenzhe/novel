89d49ea57f246484fa8b33c0392ef2694149e30b
zhangpeng <skxtay@gmail.com>
2016-12-15 17:33:45 +0800

    修复 .box

--- resources/assets/js/vendor/pages/detail.js
+++ resources/assets/js/vendor/pages/detail.js
@@ -5,12 +5,7 @@
 	(PAGETYPE == 'detail') && factory();
 }(function(){
 	$(function(){
-		var preLoad = new PreLoad({
-			el: $("#main"),
-			bookid: CHAPTER_INFO.book_id,
-			current: CHAPTER_INFO.chapter_id,
-			total: CHAPTER_INFO.chapter_num
-		});
+		var preLoad = new PreLoad();
 	})
 }))
 
